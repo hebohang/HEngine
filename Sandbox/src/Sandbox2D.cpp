c790f1a3c68567f03fb56f6287d36781e566ab3f
hebohang <hebohang1113@gmail.com>
2022-03-27 21:33:34 +0800

    Drawing Rotated Quads

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -38,13 +38,16 @@ void Sandbox2D::OnUpdate(HEngine::Timestep ts)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		HE_PROFILE_SCOPE("Renderer Draw");
 		HEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		//HEngine::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		HEngine::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		HEngine::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		HEngine::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		HEngine::Renderer2D::DrawQuad({ -5.0f,  -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		HEngine::Renderer2D::DrawQuad({ -0.5f,  -0.5f,  0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
+		HEngine::Renderer2D::DrawQuad({ 0.0f,  0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		HEngine::Renderer2D::DrawRotatedQuad({ -2.0f,  0.0f,  0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
 		HEngine::Renderer2D::EndScene();
 	}
 }

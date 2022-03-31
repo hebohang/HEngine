4293f15666614f9923d96061bf9387bd7787a50f
hebohang <hebohang1113@gmail.com>
2022-03-31 15:17:13 +0800

    change "bind" to "lambda"

--- HEngine/src/HEngine/Core/Base.h
+++ HEngine/src/HEngine/Core/Base.h
@@ -73,7 +73,7 @@
 
 #define BIT(x) (1 << x)
 
-#define HE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define HE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace HEngine
 {

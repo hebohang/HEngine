157ac1e73389d3a2a54ac147c9e06550af495809
hebohang <hebohang1113@gmail.com>
2022-04-05 15:13:45 +0800

    fix framebuffer bug

--- HEngineEditor/src/EditorLayer.cpp
+++ HEngineEditor/src/EditorLayer.cpp
@@ -84,16 +84,16 @@ namespace HEngine
     {
         HE_PROFILE_FUNCTION();
 
-        // Resize
-        //if (FramebufferSpecification spec = m_Framebuffer->GetSpecification(); 
-        //    m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && // zero sized framebuffer is invalid
-        //    (spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
-        //{
-        //    m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-        //    m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+		// Resize
+		if (FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f && // zero sized framebuffer is invalid
+			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+		{
+			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+			m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
 
-        //    m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-        //}
+			m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+		}
 
         // Update
         if (m_ViewportFocused)
@@ -214,17 +214,11 @@ namespace HEngine
         Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
         ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-        if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize) && viewportPanelSize.x > 0 & viewportPanelSize.y > 0)
-        {
-            m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
-            m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 
-            m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-
-            m_ActiveScene->OnViewportResize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
-        }
         uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
         ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+
         ImGui::End();
         ImGui::PopStyleVar();
 

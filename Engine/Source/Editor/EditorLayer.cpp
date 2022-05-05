7c72123bbc84500104403eb05ede6b35cb4594fc
hebohang <hebohang1113@gmail.com>
2022-05-05 17:11:31 +0800

    Update Editor

--- Engine/Source/Editor/EditorLayer.cpp
+++ Engine/Source/Editor/EditorLayer.cpp
@@ -20,7 +20,8 @@ namespace HEngine
 	static bool bShowSceneHierachy = true;
 	static bool bShowProperties = true;
 	static bool bShowStats = false;
-	static bool bShowSettings = true;
+	static bool bShowEngineSettings = true;
+	static bool bShowSceneSettings = true;
 
 	// Help
 	static bool bShowTutorial = false;
@@ -247,7 +248,8 @@ namespace HEngine
 				ImGui::MenuItem("Level Hierachy", NULL, &bShowSceneHierachy);
 				ImGui::MenuItem("Properties", NULL, &bShowProperties);
 				ImGui::MenuItem("Stats", NULL, &bShowStats);
-				ImGui::MenuItem("Settings", NULL, &bShowSettings);
+				ImGui::MenuItem("Engine Settings", NULL, &bShowEngineSettings);
+				ImGui::MenuItem("Environment Settings", NULL, &bShowSceneSettings);
 
 				if (ImGui::MenuItem("Load Default Layout"))
 					LoadDefaultEditorConfig();
@@ -294,9 +296,9 @@ namespace HEngine
 
 			ImGui::End();
 		}
-		if (bShowSettings)
+		if (bShowEngineSettings)
 		{
-			ImGui::Begin("Settings", &bShowSettings);
+			ImGui::Begin("Engine Settings", &bShowEngineSettings);
 			ImGui::Checkbox("Show physics colliders", &mShowPhysicsColliders);
 			const char* modes[] = { "2D", "3D" };
 			int lastMode = ModeManager::b3DMode;
@@ -307,6 +309,12 @@ namespace HEngine
 					bChangeDim = true;
 				}
 			}
+			ImGui::End();
+		}
+		if (bShowSceneSettings)
+		{
+			ImGui::Begin("Scene Settings", &bShowSceneSettings);
+
 			ImGui::End();
 		}
 		if (bShowViewport)
@@ -478,7 +486,8 @@ namespace HEngine
 		bShowSceneHierachy = true;
 		bShowProperties = true;
 		bShowStats = true;
-		bShowSettings = true;
+		bShowEngineSettings = true;
+		bShowSceneSettings = true;
 		
 		// seems imgui docking branch has some bugs with load ini file?
 
